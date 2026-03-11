6e0e7c08d22525f3c3c3132452beb31f3c119141
oliheilnany <hanis@kaist.ac.kr>
2026-03-11 15:33:00 +0000

    Update local settings.py

--- config/settings.py
+++ config/settings.py
@@ -20,15 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-'''
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4)!2_+%bbsm#!w8aw9*c(lkqc()_e3-e02h1a5@txp#@6av^!y'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-'''
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "dev-secret-key")
 DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
